54932c70956181ca4428d3fd504e17576d4a8406
DexterIV <wiedzmenpl@gmail.com>
2018-10-16 03:55:15 -0700

    A/D movement

---
+++ 3scape/Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public CharacterController2D controller;
+    public float runSpeed = 20f;
+
+    float horizontalMove = 0f;  
+	
+	// Update is called once per frame
+	void Update () {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+	}
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}

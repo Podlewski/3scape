39c6b0e5f2e8415f45d5959d47f0a1cb45f9f8eb
DexterIV <wiedzmenpl@gmail.com>
2018-10-16 04:19:17 -0700

    jump, slow on crouch, slippery colliders

--- 3scape/Assets/PlayerMovement.cs
+++ 3scape/Assets/PlayerMovement.cs
@@ -7,15 +7,25 @@ public class PlayerMovement : MonoBehaviour {
     public CharacterController2D controller;
     public float runSpeed = 20f;
 
+    bool jump = false;
+    bool crouch = false;
+
     float horizontalMove = 0f;  
 	
 	// Update is called once per frame
 	void Update () {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+        crouch = Input.GetButton("Crouch");
 	}
 
     void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
     }
 }

4bd656ef41f1dfa883a45c10c19cfbc386c66f7d
Justyna Hubert <37309986+hubjust@users.noreply.github.com>
2018-10-18 15:39:07 +0200

    Attack and enemy (layer) added.

---
+++ 3scape/Assets/Attack.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+
+    public float timeBtwAttack;
+    public float startBtwAttack;
+
+    public Transform attackPos;
+    public LayerMask whatIsEnemy;
+    public float attackRange;
+    public int damage;
+	
+	void Update () {
+		
+        if(timeBtwAttack <= 0)
+        {
+            //then you can attack
+            if (Input.GetKey(KeyCode.LeftControl))
+            {
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemy);
+                for(int i=0; i<enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+                }
+            }
+            
+            timeBtwAttack = startBtwAttack;
+        }
+
+        else
+        {
+            timeBtwAttack -= Time.deltaTime;
+        }
+	}
+
+    /*void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+
+    }*/
+}
---
+++ 3scape/Assets/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public int health;
+    public float speed;
+    private Animator anim;
+
+	void Start () {
+        //anim = GetComponent<Animator>();
+        //anim.SetBool("isRunning", true);
+	}
+	
+	void Update () {
+
+        if(health <=0)
+        {
+            Destroy(gameObject);
+        }
+
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+	}
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        Debug.Log("damage taken!");
+    }
+}

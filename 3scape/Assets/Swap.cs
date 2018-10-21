03d51fce54451e92a351ae26a7f8f90ce141229c
Karol Podlewski <karol.podlewski@hotmail.cok>
2018-10-21 12:53:48 +0200

    Character can now change their positions
    Using O it is posibble to swap beetwen 1st and 2nd position, using P it is posibble to swap between 2nd and 3rd position

---
+++ 3scape/Assets/Swap.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swap : MonoBehaviour
+{
+    public int position;
+    Vector3 newPosition;
+
+    public float timeBtwSwap;
+    public float swapTimer = 0.5f;
+
+    bool blockSwap = false;
+
+    void Update()
+    {
+        if (timeBtwSwap <= 0)
+        {
+            if (Input.GetKey(KeyCode.P))
+            {
+                if (position == 1 && blockSwap == false)
+                {
+                    position = 2;
+                    newPosition = transform.position;
+                    newPosition.x -= 1;
+                    transform.position = newPosition;
+
+                    blockSwap = true;
+                }
+
+                if (position == 2 && blockSwap == false)
+                {
+                    position = 1;
+                    newPosition = transform.position;
+                    newPosition.x += 1;
+                    transform.position = newPosition;
+
+                    blockSwap = true;
+                }
+
+                blockSwap = false;
+                timeBtwSwap = swapTimer;
+
+            }
+
+            if (Input.GetKey(KeyCode.O))
+            {
+                if (position == 2 && blockSwap == false)
+                {
+                    position = 3;
+                    newPosition = transform.position;
+                    newPosition.x -= 1;
+                    transform.position = newPosition;
+
+                    blockSwap = true;
+                }
+
+                if (position == 3 && blockSwap == false)
+                {
+                    position = 2;
+                    newPosition = transform.position;
+                    newPosition.x += 1;
+                    transform.position = newPosition;
+
+                    blockSwap = true;
+                }
+
+                blockSwap = false;
+                timeBtwSwap = swapTimer;
+            }
+        }
+
+        else
+        {
+            timeBtwSwap -= Time.deltaTime;
+        }
+    }
+
+}

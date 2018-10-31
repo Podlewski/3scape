a78c2f3f52ecbb407bcdb890fb5848a8eb99da88
shreq <shrek.kresh@o2.pl>
2018-10-31 22:36:01 +0100

    pause menu added

---
+++ 3scape/Assets/PauseMenu.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gamePaused = false;
+    public GameObject pauseMenuUI;
+    public Button ResumeB;
+    public Button RestartB;
+    public Button OptionsB;
+    public Button MenuB;
+    public Button QuitB;
+
+    void Start()
+    {
+        ResumeB.onClick.AddListener(()=>Resume());
+        RestartB.onClick.AddListener(()=>Restart());
+        OptionsB.onClick.AddListener(()=>Options());
+        MenuB.onClick.AddListener(()=>Menu());
+        QuitB.onClick.AddListener(()=>Quit());
+    }
+
+	void Update()
+    {
+		if(Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            if(gamePaused)
+                Resume();
+            else
+                Pause();
+        }
+	}
+
+    private void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        gamePaused = false;
+    }
+
+    private void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        gamePaused = true;
+    }
+
+    private void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void Options()
+    {
+        // TODO
+        //SceneManager.LoadScene(SceneManager.GetSceneByName(/*options scene name*/));
+    }
+
+    private void Menu()
+    {
+        // TODO
+        //SceneManager.LoadScene(SceneManager.GetSceneByName(/*menu scene name*/));
+    }
+
+    private void Quit()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}

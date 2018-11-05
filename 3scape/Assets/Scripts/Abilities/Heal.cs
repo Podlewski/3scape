ed8bdc62d5ac5743417045c5dcf895bec72023f5
Konrad Kowalczyk <kondzio.kowal@gmail.com>
2018-11-05 19:33:19 +0100

    Wizard is now healing team on 3 position using J key

---
+++ 3scape/Assets/Scripts/Abilities/Heal.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Heal : Ability
+{
+    public Player knight;
+    public Player mage;
+    public Player archer;
+    
+
+    void Update()
+    {
+        if (isAbilityReady())
+        {
+            animator.SetBool("IsHealing", false);
+            animator.SetBool("IsUsing", true);
+
+            if (isPressedKeyProper() && isPositionProper())
+            {
+                animator.SetBool("IsHealing", true);
+                
+                knight.Heal(5);
+                archer.Heal(5);
+                mage.Heal(5);
+                animator.SetBool("IsUsing", false);
+                setCooldown();
+            }
+        }
+
+        else
+        {
+            currentCooldown -= Time.deltaTime;
+        }
+
+    }
+}

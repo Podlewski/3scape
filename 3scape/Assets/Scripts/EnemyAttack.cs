5a48c1eb00008141b93545da79e2dce9729fda18
Karol Podlewski <karol.podlewski@hotmail.com>
2018-11-05 15:03:43 +0100

    Merge from branches feature/actualHealth and feature/destroyingObstacles

---
+++ 3scape/Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class EnemyAttack : EnemyAbility
+{
+    public Transform attackPos;
+    public LayerMask whatIsEnemy;
+    public float attackRange;
+    public int damage;
+
+    void Update()
+    {
+        if (isAbilityReady())
+        {
+           // animator.SetBool("IsAttacking", true);
+
+            Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemy);
+            for (int i = 0; i < enemiesToDamage.Length; i++)
+            {
+                enemiesToDamage[i].GetComponent<Player>().TakeDamage(damage);
+            }
+
+            setCooldown();
+            //animator.SetBool("IsAttacking", false);
+        }
+
+        else
+        {
+            currentCooldown -= Time.deltaTime;
+
+        }
+
+    }
+}

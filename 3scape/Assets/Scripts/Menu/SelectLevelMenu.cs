9b1630403431a101eb8980a31cfda649f2a91841
DexterIV <wiedzmenpl@gmail.com>
2018-11-05 12:12:05 +0100

    rearrangment

--- 3scape/Assets/Scripts/Menu/SelectLevelMenu.cs
+++ 3scape/Assets/Scripts/Menu/SelectLevelMenu.cs
@@ -13,7 +13,7 @@ public class SelectLevelMenu : MonoBehaviour
     void Start()
     {
         BackB.onClick.AddListener(() => Back());
-        LevelsB[0].onClick.AddListener(() => Select("tutorial")); // with proper scene naming these can be assigned in a loop
+        LevelsB[0].onClick.AddListener(() => Select("HealthBar")); // with proper scene naming these can be assigned in a loop
         //LevelsB[1].onClick.AddListener(() => Select("koryatrz"));
     }
 

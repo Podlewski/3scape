59a22f1023c49ba1a68e8fa5e461068851110655
Justyna Hubert <37309986+hubjust@users.noreply.github.com>
2018-11-08 21:04:31 +0100

    Unblocking chests works fine to me

---
+++ 3scape/Assets/Scripts/Abilities/UnlockingChests.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockingChests : Ability
+{
+    public float downTime, upTime, pressTime = 0;
+    public float countDown = 2.0f;
+    public bool ready = false;
+
+    public Transform unlockPos;
+    public float unlockRange;
+    public LayerMask whatCanOpen;
+
+	void Update ()
+    {
+        if (isAbilityReady())
+        {
+            //animator.SetBool("jakasAnimacja", false);
+
+                if (isPressedKeyProper() && ready == false)
+                {
+                    downTime = Time.time;
+                    pressTime = downTime + countDown;
+                    ready = true;
+                }
+
+                if (isUpKeyProper())
+                {
+                    ready = false;
+                }
+
+                if (Time.time >= pressTime && ready == true && isPositionProper())
+                {
+                    ready = false;
+
+                    //animator.SetBool("jakasAnimacja", true);
+
+                    Collider2D[] col = Physics2D.OverlapCircleAll(unlockPos.position, unlockRange, whatCanOpen);
+
+
+                    for (int i = 0; i < col.Length; i++)
+                    {
+                        if (col[i].tag == "Chest")
+                        {
+                            col[i].GetComponent<Chest>().checkIfOpen();
+                            setCooldown();
+                        }
+                    }
+                }
+        }
+
+        else
+        {
+            currentCooldown -= Time.deltaTime;
+        }
+    }
+}
--- 3scape/Assets/Scripts/Props/Chest.cs
+++ 3scape/Assets/Scripts/Props/Chest.cs
@@ -8,54 +8,18 @@ public class Chest : MonoBehaviour{
     public Sprite openSprite;
     private bool isOpen = false;
 
-    public float downTime, upTime, pressTime = 0;
-    public float countDown = 2.0f;
-    public bool ready = false;
-
-    public Transform unlockPos;
-    public float unlockRange;
-    public LayerMask whoCanOpen;
-
-    public void Update()
+    public void checkIfOpen()
     {
-        Collider2D[] col = Physics2D.OverlapCircleAll(unlockPos.position, unlockRange, whoCanOpen);
-
-        for (int i = 0; i <col.Length; i++)
+        if (isOpen)
         {
-            if (col[i].tag == "Player")
-            {
-                if (Input.GetKeyDown(KeyCode.L) && ready == false)
-                {
-                    Debug.Log("1");
-                    downTime = Time.time;
-                    pressTime = downTime + countDown;
-                    ready = true;
-                }
+            Debug.Log("opened");
+        }
 
-                if (Input.GetKeyUp(KeyCode.L))
-                {
-                    Debug.Log("2");
-                    ready = false;
-                }
-
-                if (Time.time >= pressTime && ready == true)
-                {
-                    ready = false;
-                    Debug.Log("checking");
-
-                    if (isOpen)
-                    {
-                        Debug.Log("opened");
-                    }
-
-                    else
-                    {
-                        Debug.Log("opening");
-                        isOpen = true;
-                        spriteRenderer.sprite = openSprite;
-                    }
-                }
-            }
+        else
+        {
+            Debug.Log("opening");
+            isOpen = true;
+            spriteRenderer.sprite = openSprite;
         }
     }
 }

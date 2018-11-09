d70207a7a88b071884b3a2f69951f678b3eb0af4
Justyna Hubert <37309986+hubjust@users.noreply.github.com>
2018-11-09 17:42:45 +0100

    Thorns are taking damage to players
    Everything is in scene OpeningChests

---
+++ 3scape/Assets/Scripts/Props/Thorns.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thorns : EnemyAbility
+{ 
+    public Transform attackPos;
+    public LayerMask whatIsEnemy;
+    public float attackRange;
+    public int damage;
+
+	void Update ()
+    {
+        if (isAbilityReady())
+        {
+            Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemy);
+
+            for (int i = 0; i < enemiesToDamage.Length; i++)
+            {
+                enemiesToDamage[i].GetComponent<Player>().TakeDamage(damage);
+                setCooldown();
+            }
+            
+        }
+
+        else
+        {
+            currentCooldown -= Time.deltaTime;
+
+        }
+    }
+}

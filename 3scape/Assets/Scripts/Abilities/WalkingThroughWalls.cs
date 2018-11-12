d8e1612c3b0ef00de5599c8f5789b451974da9a5
Krzysztof Barden <wiedzmenpl@gmail.com>
2018-11-12 15:49:03 +0100

    fix GetComponent looking for wrong colliders

--- 3scape/Assets/Scripts/Abilities/WalkingThroughWalls.cs
+++ 3scape/Assets/Scripts/Abilities/WalkingThroughWalls.cs
@@ -19,8 +19,7 @@ public class WalkingThroughWalls : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.K) && GlobalVariable.isMageInMiddle && isMageInviible == false)
         {
             GetComponent<Rigidbody2D>().gravityScale = 0;
-            GetComponent<BoxCollider2D>().isTrigger = true;
-            GetComponent<CircleCollider2D>().isTrigger = true;
+            GetComponent<CapsuleCollider2D>().isTrigger = true;
             GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, .5f);
 
             isMageInviible = true;
@@ -28,8 +27,7 @@ public class WalkingThroughWalls : MonoBehaviour {
         else if(Input.GetKeyDown(KeyCode.K) && GlobalVariable.isMageInMiddle && isMageInviible == true)
         {
             GetComponent<Rigidbody2D>().gravityScale = 1;
-            GetComponent<BoxCollider2D>().isTrigger = false;
-            GetComponent<CircleCollider2D>().isTrigger = false;
+            GetComponent<CapsuleCollider2D>().isTrigger = false;
             GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
 
             isMageInviible = false;

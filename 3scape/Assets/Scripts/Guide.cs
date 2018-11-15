e090b5c65456f20e0e9f878ff4a932f900387792
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2018-11-15 21:48:37 +0100

    Guide prototype

---
+++ 3scape/Assets/Scripts/Guide.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Guide : MonoBehaviour {
+    public GameObject guide;
+    public GameObject partyMember;
+    public float range;
+    public Text txt;
+
+	// Use this for initialization
+	void Start () {
+        txt.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (guide != null && partyMember != null && range > 0 && txt != null)
+        {
+            float distance = Math.Abs(guide.transform.position.y - partyMember.transform.position.y);
+            if (distance < range)
+            {
+                Debug.Log("triggered" + distance);
+                txt.enabled = true;
+            } else
+            {
+                txt.enabled = false;
+            }
+        }
+	}
+}

1ebd9da157a25e83af111721912f96e32b6b844c
Konrad Kowalczyk <kondzio.kowal@gmail.com>
2018-11-18 15:04:08 +0100

    Add basic HUD appearance and change champions' color to green when healing

--- 3scape/Assets/Scripts/Characters/Player.cs
+++ 3scape/Assets/Scripts/Characters/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
     public float startHealth;
     public float health;
     public Image healthBar;
+    public Image healthBarHUD;
 
     private bool physicalImmunity = false;
     private bool magicImmunity = false;
@@ -41,6 +42,7 @@ public class Player : MonoBehaviour
     {
         health -= damage;
         healthBar.fillAmount = health / startHealth;
+        healthBarHUD.fillAmount = health / startHealth;
         Debug.Log(this.gameObject.name + ": " + damage + " damage was taken!");
     }
 
@@ -76,9 +78,10 @@ public class Player : MonoBehaviour
     public void HealAnimation()
     {
         float waitTime = 1.0f;
-        if (healthBar.fillAmount < health / startHealth)
+        if ((healthBar.fillAmount < health / startHealth) || (healthBarHUD.fillAmount < health / startHealth))
         {
             healthBar.fillAmount += 1.0f / waitTime * Time.deltaTime;
+            healthBarHUD.fillAmount += 1.0f / waitTime * Time.deltaTime;
         }
         else
         {

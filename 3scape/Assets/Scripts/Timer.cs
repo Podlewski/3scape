c2057a366b481237b8ac4eb381f9067681f6ee59
DexterIV <wiedzmenpl@gmail.com>
2018-11-18 21:07:46 +0100

    Timer on Tutorial Level

---
+++ 3scape/Assets/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+
+
+    private float startingTime;
+    private Text timerText;
+	// Use this for initialization
+	void Start () {
+        timerText = GetComponent<Text>();
+        startingTime = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        startingTime += Time.deltaTime;
+
+        timerText.text = "" + Mathf.Round(startingTime) + "s";
+	}
+}

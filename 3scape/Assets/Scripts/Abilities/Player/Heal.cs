7395885fe12275ef03dce30bc92c62160c7b489a
Karol Podlewski <karol.podlewski@hotmail.com>
2018-11-20 00:31:44 +0100

    Typo in tutorial, removed unnecessary fields from Heal
    Was trying to fix healing, but i am to sleapy

--- 3scape/Assets/Scripts/Abilities/Player/Heal.cs
+++ 3scape/Assets/Scripts/Abilities/Player/Heal.cs
@@ -9,13 +9,6 @@ public class Heal : PlayerAbility
     private Player mage;
     private Player archer;
 
-    private SpriteRenderer srKnight;
-    private SpriteRenderer srMage;
-    private SpriteRenderer srArcher;
-
-    private Color32 playerColor = new Color32(255, 255, 255, 225);
-    public Color32 magicColor = new Color32(255, 18, 234, 14);
-
     public Image FirstSkillCoolDown;
 
     private void Start()
@@ -23,11 +16,6 @@ public class Heal : PlayerAbility
         knight = GameObject.Find("knight").GetComponent<Player>();
         mage = GameObject.Find("mage").GetComponent<Player>();
         archer = GameObject.Find("archer").GetComponent<Player>();
-
-        srKnight = GameObject.Find("knight").GetComponent<SpriteRenderer>();
-        srMage = GameObject.Find("mage").GetComponent<SpriteRenderer>();
-        srArcher = GameObject.Find("archer").GetComponent<SpriteRenderer>();
-
     }
 
     void Update()
@@ -41,18 +29,13 @@ public class Heal : PlayerAbility
             {
                 animator.SetBool("IsHealing", true);
 
-                srKnight.color = magicColor;
-                srMage.color = magicColor;
-                srArcher.color = magicColor;
-
-
                 knight.isBeingHealed = true;
                 archer.isBeingHealed = true;
                 mage.isBeingHealed = true;
 
-                knight.Heal(healValue);
-                archer.Heal(healValue);
                 mage.Heal(healValue);
+                archer.Heal(healValue);
+                knight.Heal(healValue);
                 //animator.SetBool("IsUsing", false);
 
                 setCooldown();
@@ -62,9 +45,6 @@ public class Heal : PlayerAbility
         }
         else if (!isAbilityStillWorking() || !isPositionProper())
         {
-            srKnight.color = playerColor;
-            srMage.color = playerColor;
-            srArcher.color = playerColor;
             reduceCooldown();
 
             FirstSkillCoolDown.fillAmount = currentCooldown / cooldown;

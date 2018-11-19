a0a4938360b04b2440753289559add93e9c83cac
Krzysztof Barden <wiedzmenpl@gmail.com>
2018-11-19 18:35:03 +0100

    camera is now in the middle of the party

---
+++ 3scape/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+
+    public Transform playerAveragePosition;
+    public Transform knight;
+    public Transform mage;
+    public Transform archer;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerAveragePosition.position = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vec = Vector3.zero;
+        vec = (knight.position + mage.position + archer.position) / 3;
+        playerAveragePosition.position = vec;
+    }
+}

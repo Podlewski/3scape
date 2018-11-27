23fad1b21ade492cc707dd2137ac22844550f201
Krzysztof Barden <wiedzmenpl@gmail.com>
2018-11-27 21:40:10 +0100

    AI v2

--- 3scape/Assets/Scripts/Characters/Enemy.cs
+++ 3scape/Assets/Scripts/Characters/Enemy.cs
@@ -3,32 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour
+{
 
     public float startHealth;
     private float health;
 
     public float speed;
     public Image optionalHealthBar;
-    public Transform characters;
 
-    void Start () {
+    bool m_FacingLeft = true;
+    void Start()
+    {
         health = startHealth;
     }
-	
-	void Update () {
 
-        if(health <=0)
+    void Update()
+    {
+        if (health <= 0)
         {
             Destroy(gameObject);
         }
+    }
 
-        if (Physics2D.Raycast(transform.position, Vector2.left, 7, 1 << 8 /*player layerMask*/)) {
-            transform.Translate(Vector2.left * speed * Time.deltaTime);
-        }
-        if (Physics2D.Raycast(transform.position, Vector2.right, 7, 1 << 8 /*player layerMask*/))
+    void FixedUpdate()
+    {
+        if (GetComponent<Animator>() != null)
         {
-            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            if (!(Physics2D.Raycast(transform.position, Vector2.left, 0.3f, 1 << 8 /*player layerMask*/)
+                           || (Physics2D.Raycast(transform.position, Vector2.right, 0.3f, 1 << 8 /*player layerMask*/))))
+            {
+                GetComponent<Animator>().SetFloat("Speed", speed);
+                if (Physics2D.Raycast(transform.position, Vector2.left, 7, 1 << 8 /*player layerMask*/))
+                {
+                    if (!m_FacingLeft)
+                        Flip();
+                    m_FacingLeft = true;
+                    transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+                }
+                if (Physics2D.Raycast(transform.position, Vector2.right, 7, 1 << 8 /*player layerMask*/))
+                {
+                    Flip();
+                    transform.Translate(Vector2.left * speed * Time.deltaTime);
+                }
+            }
+            else
+            {
+                GetComponent<Animator>().SetFloat("Speed", 0);
+            }
         }
     }
 
@@ -40,5 +63,14 @@ public class Enemy : MonoBehaviour {
             optionalHealthBar.fillAmount = health / startHealth;
 
         Debug.Log(damage + " damage was taken!");
+    }
+
+    void Flip()
+    {
+        if (m_FacingLeft)
+        {
+            transform.Rotate(0f, 180f, 0f);
+            m_FacingLeft = false;
+        }
     }
 }

78249e37882a5242fbbb964d89585db4c7c5a966
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2018-12-02 22:43:07 +0100

    Setting traps works, traps take damage, no animation yet

---
+++ 3scape/Assets/Scripts/Abilities/Player/SettingTraps.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingTraps : ColorAbility
+{
+    public GameObject trap;
+    public Transform settingPoint;
+	
+	void Update () {
+        if (isAbilityReady() && isPositionProper() && isButtonPressedProper())
+        {
+            SetTrap();
+            setCooldown();
+        }
+
+        if(!isPositionProper() || !isAbilityStillWorking())
+        {
+            reduceCooldown();
+        }
+	}
+
+    void SetTrap()
+    {
+        Instantiate(trap, settingPoint.position, settingPoint.rotation);
+    }
+}
---
+++ 3scape/Assets/Scripts/Props/Trap.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    public GameObject explosion;
+
+    public Transform trapPos;
+    public LayerMask whatIsEnemy;
+    public float explosionRange;
+    public int damage;
+
+    private bool triggered = false;
+	
+	// Update is called once per frame
+	void Update () {
+        Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(trapPos.position, explosionRange, whatIsEnemy);
+
+        if (!triggered && enemiesToDamage.Length > 0 )
+        {
+            for (int i = 0; i < enemiesToDamage.Length; i++)
+            {
+                Debug.Log(i);
+                triggered = true;
+                //Instantiate(explosion, trapPos.position);
+                Destroy(gameObject);
+                enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+            }
+  
+        }
+    }
+}

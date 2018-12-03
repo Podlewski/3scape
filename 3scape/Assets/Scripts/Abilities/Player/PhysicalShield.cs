cbbe5e958fc988fdc4462f7105bcae08b803cbed
shreq <shrek.kresh@o2.pl>
2018-12-04 00:41:59 +0100

    another hud fix

--- 3scape/Assets/Scripts/Abilities/Player/PhysicalShield.cs
+++ 3scape/Assets/Scripts/Abilities/Player/PhysicalShield.cs
@@ -1,8 +1,27 @@
-﻿using UnityEngine.UI;
+﻿using UnityEngine;
+using UnityEngine.UI;
 public class PhysicalShield : ColorAbility
 {
     CharacterController2D characterController2D;
-    public Image SecondSkillCoolDown;
+
+    public Image FirstSkillCoolDown;
+    private Color defaultColor;
+    private bool defaultDirection;
+
+    void Start()
+    {
+        knight = GameObject.Find("knight").GetComponent<Player>();
+        archer = GameObject.Find("archer").GetComponent<Player>();
+        mage = GameObject.Find("mage").GetComponent<Player>();
+
+        srKnight = GameObject.Find("knight").GetComponent<SpriteRenderer>();
+        srArcher = GameObject.Find("archer").GetComponent<SpriteRenderer>();
+        srMage = GameObject.Find("mage").GetComponent<SpriteRenderer>();
+
+        defaultColor = FirstSkillCoolDown.color;
+        defaultDirection = FirstSkillCoolDown.fillClockwise;
+    }
+
     void Update()
     {
         if (isAbilityReady())
@@ -13,7 +32,7 @@ public class PhysicalShield : ColorAbility
                 knight.SetPhysicalImmunity();
                 SetAbilityColor(true, false, false);
                 setCooldown();
-                // SecondSkillCoolDown.fillAmount = 1;
+                FirstSkillCoolDown.fillAmount = 1;
             }
         }
         else if (!isAbilityStillWorking() || !isPositionProper())
@@ -22,12 +41,25 @@ public class PhysicalShield : ColorAbility
             knight.DisbalePhysicalImmunity();
             BackToNormalColor(true, false, false);
             reduceCooldown();
-            // SecondSkillCoolDown.fillAmount = currentCooldown / cooldown;
+            // FirstSkillCoolDown.fillAmount = currentCooldown / cooldown;
         }
 
-        //if (!isPositionProper())
-        //    SecondSkillCoolDown.fillAmount = 1;
-        //else
-        //    SecondSkillCoolDown.fillAmount = currentCooldown / cooldown;
+        if (isAbilityStillWorking())
+        {
+            FirstSkillCoolDown.color = new Color(0.5f, 0.2f, 0.7f, 0.8f);
+            FirstSkillCoolDown.fillClockwise = !defaultDirection;
+            FirstSkillCoolDown.fillAmount = remaindingDuration / duration;
+        }
+        else
+        {
+            FirstSkillCoolDown.color = defaultColor;
+            FirstSkillCoolDown.fillClockwise = defaultDirection;
+            FirstSkillCoolDown.fillAmount = currentCooldown / cooldown;
+        }
+
+        if (!isPositionProper())
+            FirstSkillCoolDown.fillAmount = 1;
+        else if (!isAbilityStillWorking())
+            FirstSkillCoolDown.fillAmount = currentCooldown / cooldown;
     }
 }

a4eaa5c54b5d8746acf23de7c2bc39105df5566b
Karol Podlewski <karol.podlewski@hotmail.com>
2018-12-04 00:22:52 +0100

    I have no idea

--- 3scape/Assets/Scripts/Abilities/Player/PhysicalShield.cs
+++ 3scape/Assets/Scripts/Abilities/Player/PhysicalShield.cs
@@ -1,20 +1,8 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
-
+﻿using UnityEngine.UI;
 public class PhysicalShield : ColorAbility
 {
     CharacterController2D characterController2D;
-
-    public Image FirstSkillCoolDown;
-    private Color defaultColor;
-    private bool defaultDirection;
-
-    void Start()
-    {
-        defaultColor = FirstSkillCoolDown.color;
-        defaultDirection = FirstSkillCoolDown.fillClockwise;
-    }
-
+    public Image SecondSkillCoolDown;
     void Update()
     {
         if (isAbilityReady())
@@ -22,46 +10,24 @@ public class PhysicalShield : ColorAbility
             if (isButtonDownProper() && isPositionProper())
             {
                 PlayerMovement.runSpeed = 10f;
-
                 knight.SetPhysicalImmunity();
-
                 SetAbilityColor(true, false, false);
-
                 setCooldown();
-
-                FirstSkillCoolDown.fillAmount = 1;
+                // SecondSkillCoolDown.fillAmount = 1;
             }
         }
-
         else if (!isAbilityStillWorking() || !isPositionProper())
         {
             PlayerMovement.runSpeed = 20f;
-
             knight.DisbalePhysicalImmunity();
-
             BackToNormalColor(true, false, false);
-
             reduceCooldown();
-
-            //FirstSkillCoolDown.fillAmount = currentCooldown / cooldown;
+            // SecondSkillCoolDown.fillAmount = currentCooldown / cooldown;
         }
 
-        if (isAbilityStillWorking())
-        {
-            FirstSkillCoolDown.color = new Color(0.5f, 0.2f, 0.7f, 0.8f);
-            FirstSkillCoolDown.fillClockwise = !defaultDirection;
-            FirstSkillCoolDown.fillAmount = remaindingDuration / duration;
-        }
-        else
-        {
-            FirstSkillCoolDown.color = defaultColor;
-            FirstSkillCoolDown.fillClockwise = defaultDirection;
-            FirstSkillCoolDown.fillAmount = currentCooldown / cooldown;
-        }
-
-        if (!isPositionProper())
-            FirstSkillCoolDown.fillAmount = 1;
-        else if (!isAbilityStillWorking())
-            FirstSkillCoolDown.fillAmount = currentCooldown / cooldown;
+        //if (!isPositionProper())
+        //    SecondSkillCoolDown.fillAmount = 1;
+        //else
+        //    SecondSkillCoolDown.fillAmount = currentCooldown / cooldown;
     }
 }

eba079021ddbbbb14330701ad3ac2ca994564e67
Karol Podlewski <karol.podlewski@hotmail.com>
2018-12-06 18:23:18 +0100

    First changes to menu + new icons finished

--- 3scape/Assets/Scripts/Menu/MainMenu.cs
+++ 3scape/Assets/Scripts/Menu/MainMenu.cs
@@ -7,45 +7,51 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public GameObject mainMenuUI;
-    public Button ContinueB;
     public Button NewGameB;
-    public Button SelectLevelB;
-    public Button TutorialB;
+    public Button LoadGameB;
     public Button OptionsB;
+    public Button CreditsB;
     public Button QuitB;
+
+    public GameObject selectLevelUI;
+    public Button TutorialSLB;
+    public Button Level01SLB;
+    public Button BackSLB;
 
     void Start()
     {
-        ContinueB.onClick.AddListener(() => Continue());
-        NewGameB.onClick.AddListener(() => NewGame());
-        SelectLevelB.onClick.AddListener(() => SelectLevelMenu());
-        TutorialB.onClick.AddListener(() => StartTutorial());
+        NewGameB.onClick.AddListener(() => SelectLevelMenu());
+        LoadGameB.onClick.AddListener(() => LoadGameMenu());
         OptionsB.onClick.AddListener(() => Options());
+        CreditsB.onClick.AddListener(() => Credits());
         QuitB.onClick.AddListener(() => Quit());
+
+        TutorialSLB.onClick.AddListener(() => SLTutorial());
+        Level01SLB.onClick.AddListener(() => SLLevel01());
+        BackSLB.onClick.AddListener(() => SLBack());
     }
 
-    //void Update() { }
-
-    private void Continue() { }
-
-    private void NewGame()
-    {
-        SceneManager.LoadScene("Level01", LoadSceneMode.Single);
-    }
+    // Main Menu
 
     private void SelectLevelMenu()
     {
-        SceneManager.LoadSceneAsync("select_level_menu", LoadSceneMode.Single);
+        selectLevelUI.gameObject.SetActive(true);
+        mainMenuUI.gameObject.SetActive(false);
     }
 
-    private void StartTutorial()
+    private void LoadGameMenu()
     {
-        SceneManager.LoadScene("Tutorial", LoadSceneMode.Single);
+        //SceneManager.LoadSceneAsync("load_game_menu", LoadSceneMode.Single);
     }
 
     private void Options()
     {
         SceneManager.LoadScene("settings_menu", LoadSceneMode.Single);
+    }
+
+    private void Credits()
+    {
+        //SceneManager.LoadScene("credits_menu", LoadSceneMode.Single);
     }
 
     private void Quit()
@@ -56,4 +62,24 @@ public class MainMenu : MonoBehaviour
             Application.Quit();
         #endif
     }
+
+    // Select Level
+
+    private void SLTutorial()
+    {
+        SceneManager.LoadSceneAsync("Tutorial", LoadSceneMode.Single);
+    }
+
+    private void SLLevel01()
+    {
+        SceneManager.LoadSceneAsync("Level01", LoadSceneMode.Single);
+    }
+
+    private void SLBack()
+    {
+        mainMenuUI.gameObject.SetActive(true);
+        selectLevelUI.gameObject.SetActive(false);
+    }
+
+
 }

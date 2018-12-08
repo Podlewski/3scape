eddebcfa61ca6ce1f38d109ee0ad66558fbd7ba8
Karol Podlewski <karol.podlewski@hotmail.com>
2018-12-08 13:00:34 +0100

    Rearrangement + first version of new Guide

---
+++ 3scape/Assets/Scripts/Another/Guide.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Guide : MonoBehaviour
+{
+    public GameObject guide;
+    private GameObject partyPosition;
+    public Text txt;
+    public float range = 5;
+    
+    public string text1;
+    public string position;
+    public string text2;
+    public string keyCode; 
+    public string text3;
+
+    // Use this for initialization
+    void Start()
+    {
+        fillText();
+        partyPosition = GameObject.Find("playerAvgPos");
+
+        if (txt != null) txt.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (guide != null && partyPosition != null && range > 0 && txt != null)
+        {
+            float distance = Math.Abs(guide.transform.position.x - partyPosition.transform.position.x);
+            if (distance < range)
+            {
+                //Debug.Log("triggered" + distance);
+                txt.enabled = true;
+                //txt.color = new Color(1, 1, 1, 0.5f);
+            }
+            else
+            {
+                txt.enabled = false;
+            }
+        }
+    }
+
+    void fillText()
+    {
+        txt.text = text1 + " " + position + " " + text2 + " " + keyCode + " " + text3;
+    }
+}
---
+++ 3scape/Assets/Scripts/Another/GuideText.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class GuideText : MonoBehaviour
+{
+    public string text1;
+    public string text2;
+    public string text3;
+
+
+}

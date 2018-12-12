b2cdb0e167f7e366ce485afe5dd0a6b68975efb7
Krzysztof Barden <wiedzmenpl@gmail.com>
2018-12-12 16:34:08 +0100

    black knight RETURNS

---
+++ 3scape/Assets/Scripts/Abilities/Enemy/BlackKnight.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class BlackKnight : AnimatedAbility
+{
+    public LayerMask whatIsEnemy;
+    public float attackRange;
+    public int damage;
+
+    public AudioClip zombieSound;
+    public AudioSource source;
+
+    private bool start = true;
+
+    void Start()
+    {
+        source.clip = zombieSound;
+    }
+
+    void Update()
+    {
+        if ((Physics2D.Raycast(transform.position, Vector2.left, attackRange, whatIsEnemy)
+          || Physics2D.Raycast(transform.position, Vector2.right, attackRange, whatIsEnemy)))
+        {
+            animator.SetTrigger("IsAttacking");
+
+            if (isAbilityReady())
+            {
+                setCooldown();
+
+                if (!start)
+                {
+                    var enemiesToDamage = Physics2D.OverlapCircleAll(gameObject.transform.Find("AttackPoint").position, attackRange + 2.2f, 1 << 8);
+                    foreach (var enemy in enemiesToDamage)
+                    {
+                        enemy.GetComponent<Player>().TakePhysicalDamage(damage);
+                    }
+
+                    source.Play();
+                }
+                start = false;
+            }
+            else
+            {
+                currentCooldown -= Time.deltaTime;
+            }
+        }
+
+        else
+        {
+            currentCooldown -= Time.deltaTime;
+            animator.ResetTrigger("IsAttacking");
+        }
+
+    }
+}

7f688ba22b9d239e2ee9ad4fa7551398d3717421
Karol Podlewski <karol.podlewski@hotmail.com>
2019-01-11 19:02:33 +0100

    Final polishing of credits (hope so)

--- 3scape/Assets/Credits.cs
+++ 3scape/Assets/Credits.cs
@@ -1,18 +1,31 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class Credits : MonoBehaviour
 {
     public Button BackToMenuB;
+    private bool reset;
 
     void Start ()
     {
+        reset = true;
+        StartCoroutine(TimeCounter());
         BackToMenuB.onClick.AddListener(() => BackToMenu());
     }
 
     private void BackToMenu()
     {
+        reset = false;
         SceneManager.LoadScene("main_menu", LoadSceneMode.Single);
     }
+
+    private IEnumerator TimeCounter()
+    {
+        yield return new WaitForSeconds(22f);
+        if (reset == true)
+            BackToMenu();
+    }
+
 }

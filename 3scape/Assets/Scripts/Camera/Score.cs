5945eca431bb576f76bfca80debce436ee3e0478
Justyna <justyna.hubert@interia.pl>
2019-01-14 19:10:50 +0100

    Victory canvas added and fixes in level01

--- 3scape/Assets/Scripts/Camera/Score.cs
+++ 3scape/Assets/Scripts/Camera/Score.cs
@@ -6,12 +6,12 @@ public class Score : MonoBehaviour
     //public float slots = 100;
 
     public Text levelScore;
-    public Text levelScoreAtTheEndOfLevel;
+    //public Text levelScoreAtTheEndOfLevel;
     public int score = 0;
 
     private void Update()
     {
         levelScore.text = score.ToString();
-        levelScoreAtTheEndOfLevel.text = "Your score: " + score.ToString();
+        //levelScoreAtTheEndOfLevel.text = "Your score: " + score.ToString();
     }
 }

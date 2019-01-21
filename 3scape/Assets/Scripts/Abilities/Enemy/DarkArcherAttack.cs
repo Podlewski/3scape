bb8dab687efe8636cf9b6055d8f4f6afdfd14979
Gracjan Grala <gracjan.grala.tel@gmail.com>
2019-01-21 22:10:08 +0100

    Dark Bowman added

---
+++ 3scape/Assets/Scripts/Abilities/Enemy/DarkArcherAttack.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkArcherAttack : AnimatedAbility {
+
+    public LayerMask whatIsEnemy;
+    public float attackRange;
+    public int damage;
+
+    public Transform firePoint;
+    public GameObject arrowPrefab;
+
+    private Color defaultColor;
+    private bool defaultDirection;
+
+    public AudioClip shot;
+    public AudioSource source;
+
+    void Update()
+    {
+        if ((Physics2D.Raycast(transform.position, Vector2.left, attackRange, whatIsEnemy /*player layerMask*/)
+          || Physics2D.Raycast(transform.position, Vector2.right, attackRange, whatIsEnemy/*player layerMask*/)))
+        {
+            animator.SetBool("IsAttacking", true);
+            if (isAbilityReady())
+            {
+                setCooldown();
+                var enemiesToDamage = Physics2D.OverlapCircleAll(gameObject.transform.Find("DarkArcherFirePoint").position, attackRange, 1 << 8);
+                foreach (var enemy in enemiesToDamage)
+                {
+                    //enemy.GetComponent<Player>().TakePhysicalDamage(damage);
+                    Shoot();
+                }
+            }
+            else
+            {
+                currentCooldown -= Time.deltaTime;
+            }
+
+        }
+
+        else
+        {
+            currentCooldown -= Time.deltaTime;
+            animator.SetBool("IsAttacking", false);
+        }
+
+    }
+
+    void Shoot()
+    {
+        Instantiate(arrowPrefab, firePoint.position, firePoint.rotation);
+        if (source != null && shot != null)
+        {
+            source.PlayOneShot(shot);
+        }
+    }
+}
---
+++ 3scape/Assets/Scripts/Props/EnemyArrow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyArrow : MonoBehaviour {
+
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    public int damage;
+
+    private float startTime;
+
+    void Start()
+    {
+        startTime = Time.time;
+        rb.velocity = -transform.right * speed;
+    }
+
+    void Update()
+    {
+        if (Time.time - startTime > 10)
+            Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Debug.Log(hitInfo.name + " " + damage);
+
+        if (hitInfo.GetComponent<Player>() != null)
+        {
+            hitInfo.GetComponent<Player>().TakeDamage(damage);
+            Destroy(gameObject);
+        }
+
+    }
+}
